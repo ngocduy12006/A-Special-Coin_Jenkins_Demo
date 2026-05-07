6574dd8f20605721b222405ba3d613d4f7800005
Nguyễn Ngọc Duy <duy3012006@MacBook-Pro-cua-Nguyen-3.local>
2026-05-07 19:19:44 +1000

    update3

--- temperature/EmptyClass.cs
+++ temperature/EmptyClass.cs
@@ -2,6 +2,6 @@
 {
     public static void main(string[] args)
     {
-        Console.WriteLine("Hello World, my name is Linda"); 
+        Console.WriteLine("Hello World, I am from Vietnam"); 
     }
 }
