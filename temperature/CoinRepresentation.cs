f5ec5c8070c8471de296a07fbeaf50ffd712c049
Nguyễn Ngọc Duy <duy3012006@MacBook-Pro-cua-Nguyen-3.local>
2026-05-07 16:16:40 +1000

    Initial commit

---
+++ temperature/CoinRepresentation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CoinRepresentation
+{
+    public class CoinRepresentation
+    {
+        private static Dictionary<long, long> Z = new Dictionary<long, long>();
+        private static bool isEven(long sum)
+        {
+            if (sum % 2 == 0) return true;
+            return false;
+        }
+
+        public static long Solve(long sum)
+        {
+            if (sum == 1 || sum == 0) return 1;
+            if (Z.ContainsKey(sum))
+            {
+                return Z[sum];
+            }
+            long result;
+            if (isEven(sum) == false)
+            {
+                 result =  Solve((sum - 1) / 2);
+            }
+            else
+            {
+                 result = Solve(sum / 2) + Solve((sum -2)/2);
+            }
+
+            Z[sum] = result;
+            return result; 
+        }
+
+    }
+}
